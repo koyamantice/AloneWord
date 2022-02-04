c512f28ba5ec84907d92ccf7f468947680c9d13d
koyamakaito <k020g1197@g.neec.ac.jp>
2022-02-04 22:57:32 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GamePlayScene.cpp
@@ -0,0 +1,61 @@
+#include "GamePlayScene.h"
+#include "Audio.h"
+#include "input.h"
+#include "DebugText.h"
+#include "TitleScene.h"
+
+void GamePlayScene::Initialize() {
+	//オブジェクト初期化
+	// テクスチャ読み込み
+	Sprite::LoadTexture(1, L"Resources/2d/title.png");
+	Sprite::LoadTexture(2, L"Resources/2d/gameplay.png");
+
+	//背景スプライト生成
+	sprite = Sprite::Create(2, { 0.0f,0.0f });
+	//スプライト生成
+
+	// モデル読み込み
+	Audio::GetInstance()->LoadSound(1, "Resources/BGM/NewWorld.wav");
+	srand(NULL);
+}
+
+void GamePlayScene::Finalize() {
+	//３ｄのモデルのデリート
+}
+
+void GamePlayScene::Update() {
+	Input* input = Input::GetInstance();
+	if (input->TriggerKey(DIK_Z)) {
+		Audio::GetInstance()->StopWave(0);
+		Audio::GetInstance()->StopWave(1);
+		Audio::GetInstance()->LoopWave(0,0.7f);
+	}
+	if (input->TriggerKey(DIK_C)) {
+		Audio::GetInstance()->StopWave(0);
+		Audio::GetInstance()->StopWave(1);
+		Audio::GetInstance()->LoopWave(1,0.7f);
+	}
+	if (input->TriggerKey(DIK_SPACE)) {
+		Audio::GetInstance()->StopWave(0);
+		Audio::GetInstance()->StopWave(1);
+		SceneManager::GetInstance()->ChangeScene("TITLE");
+	}
+	DebugText::GetInstance()->Print("SPACE to TITLE!!",200, 100,1.0f);
+	DebugText::GetInstance()->Print("Z or C to Sound!!", 200, 115, 1.0f);
+}
+
+void GamePlayScene::Draw() {
+	Sprite::PreDraw();
+	//背景用
+	sprite->Draw();
+
+
+	Object3d::PreDraw();
+	//背景用
+
+
+
+	//前面用
+
+}
+
---
+++ scene/GamePlayScene.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "BaseScene.h"
+#include "Sprite.h"
+#include "Object3d.h"
+
+#include<memory>
+
+/// <summary>
+/// ゲームプレイシーン
+/// </summary>
+class GamePlayScene : public BaseScene{
+public:
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize() override;
+	/// <summary>
+	/// 終了
+	/// </summary>
+	void Finalize() override;
+
+	/// <summary>
+	/// 毎フレーム更新
+	/// </summary>
+	void Update() override;
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw() override;
+
+private:
+	Model* modelPin = nullptr;
+	Object3d *objPin;
+	Sprite* sprite = { nullptr };
+
+
+};
+
