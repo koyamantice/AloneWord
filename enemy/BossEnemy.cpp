91aa7a07b70f8d2c7688c310d828a1e27dd1e1c8
koga_manami <k020g1416@g.neec.ac.jp>
2022-03-23 12:09:25 +0900

    衝突起きたので直しました!

--- enemy/BossEnemy.cpp
+++ enemy/BossEnemy.cpp
@@ -214,12 +214,9 @@ void BossEnemy::Fork() {
 						already = true;
 					}
 				}
-<<<<<<< HEAD
 			}
-			else {
-=======
+		
 			} else {
->>>>>>> 431169a03ceb5a4a5284baeaca8df134e6dd3be0
 				AfterPos.y = 0.0f;
 				pos = {
 					pos.x,
@@ -263,10 +260,6 @@ void BossEnemy::Fork() {
 		}
 	}
 
-
-
-
-}
 
 //bool BossEnemy::collidePlayer(Player* player) {
 //	XMFLOAT3 pos = player->GetPosition();
