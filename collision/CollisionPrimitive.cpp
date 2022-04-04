db8893c533e0ba650a7e768425028970d95f80af
IkeuchiYura <k020g1233@g.neec.ac.jp>
2022-04-04 14:31:59 +0900

    メッシュ準備

--- collision/CollisionPrimitive.cpp
+++ collision/CollisionPrimitive.cpp
@@ -2,12 +2,12 @@
 
 using namespace DirectX;
 
-void Triangle::ComputeNormal() {
-
+void Triangle::ComputeNormal()
+{
 	XMVECTOR p0_p1 = p1 - p0;
-	XMVECTOR p1_p2 = p2 - p0;
-	
-	//外積により、二辺に垂直なベクトル
-	normal = XMVector3Cross(p0_p1, p1_p2);
+	XMVECTOR p0_p2 = p2 - p0;
+
+	//外積により、2辺に垂直なベクトルを算出する
+	normal = XMVector3Cross(p0_p1, p0_p2);
 	normal = XMVector3Normalize(normal);
 }
--- collision/CollisionPrimitive.h
+++ collision/CollisionPrimitive.h
@@ -1,53 +1,52 @@
 #pragma once
-/// <summary>
-/// 当たり判定プリミティブ
-/// </summary>
 
-#include<DirectXMath.h>
-	/// <summary>
-	/// 球
-	/// </summary>
-struct Sphere {
+#include <DirectXMath.h>
+
+using namespace DirectX;
+//球
+struct Sphere
+{
 	//中心座標
-	DirectX::XMVECTOR center = { 0,0,0,1 };
+	XMVECTOR center = { 0,0,0,1 };
 	//半径
 	float radius = 1.0f;
 };
-/// <summary>
-/// 平面
-/// </summary>
-struct Plane {
+
+//平面
+struct Plane
+{
 	//法線ベクトル
-	DirectX::XMVECTOR normal = { 0,1,0,0 };
+	XMVECTOR normal = { 0,1,0,0 };
 	//原点(0,0,0)からの距離
-	float distance = 1.0f;
+	float distance = 0.0f;
 };
-/// <summary>
-/// 法線付き三角形（時計回りが表面）
-/// </summary>
-struct Triangle {
+
+//法線付き三角形
+class Triangle
+{
 public:
-	void ComputeNormal();
-	//頂点座標
-	DirectX::XMVECTOR p0;
-	DirectX::XMVECTOR p1;
-	DirectX::XMVECTOR p2;
+	//頂点座標　3つ
+	XMVECTOR p0;
+	XMVECTOR p1;
+	XMVECTOR p2;
+
 	//法線ベクトル
-	DirectX::XMVECTOR normal;
+	XMVECTOR normal;
+
+	//法線の計算	
+	void ComputeNormal();
 };
-struct Ray 	{
-	DirectX::XMVECTOR start = { 0,0,0,1 };
-	DirectX::XMVECTOR dir = { 1,0,0,0 };
-};
-class CollisionPrimitive {
+
+//レイ
+class Ray
+{
 public:
-	/// <summary>
-	/// 当たり判定ヘルパークラス
-	/// </summary>
-
-
-
-
-
+	//始点座標
+	XMVECTOR start = { 0,0,0,1 };
+	//方向
+	XMVECTOR dir = { 1,0,0,0 };
 };
+
+
+
 
