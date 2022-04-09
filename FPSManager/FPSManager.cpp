1a72115f76b6c8e5f363f2e381e814ed5b743b63
IkeuchiYura <k020g1233@g.neec.ac.jp>
2022-04-09 22:49:03 +0900

    FPS準備

---
+++ FPSManager/FPSManager.cpp
@@ -0,0 +1,44 @@
+#include "FPSManager.h"
+
+HRESULT FPSManager::Initialize() {
+	// メインループ前の輝度取得
+	if (QueryPerformanceFrequency(&timeFreq) == FALSE) { 
+		return(E_FAIL);
+	}
+	//初回計算用
+	QueryPerformanceCounter(&timeStart);
+}
+
+void FPSManager::Run() {
+	// 今の時間を取得
+	QueryPerformanceCounter(&timeEnd);
+	// (今の時間 - 前フレームの時間) / 周波数 = 経過時間(秒単位)
+	frameTime = static_cast<float>(timeEnd.QuadPart - timeStart.QuadPart) / static_cast<float>(timeFreq.QuadPart);
+
+	if (frameTime < MIN_FREAM_TIME) { // 時間に余裕がある
+		// ミリ秒に変換
+		DWORD sleepTime = static_cast<DWORD>((MIN_FREAM_TIME - frameTime) * 1000);
+
+		timeBeginPeriod(1); // 分解能を上げる(こうしないとSleepの精度はガタガタ)
+		Sleep(sleepTime);   // 寝る
+		timeEndPeriod(1);   // 戻す
+
+		return;
+	}
+
+	if (frameTime > 0.0) { // 経過時間が0より大きい(こうしないと下の計算でゼロ除算になると思われ)
+		fps = (fps * 0.99f) + (0.01f / frameTime); // 平均fpsを計算
+#ifdef _DEBUG
+// デバッグ用(デバッガにFSP出す)
+#ifdef UNICODE
+		std::wstringstream stream;
+#else
+		std::stringstream stream;
+#endif
+		stream << fps << " FPS" << std::endl;
+		// カウンタ付けて10回に1回出力
+		OutputDebugString(stream.str().c_str());
+#endif
+	}
+	timeStart = timeEnd; // 入れ替え
+}
