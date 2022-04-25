181cb8cf99ebb010c0cb704b370413f9f37cc6aa
IkeuchiYura <k020g1233@g.neec.ac.jp>
2022-04-25 14:16:48 +0900

    もう一回

--- InterBoss.cpp
+++ InterBoss.cpp
@@ -29,7 +29,6 @@ void InterBoss::Draw() {
 	//ImGui::SliderFloat("speed_y", &targetpos.x, 25, -25);
 	ImGui::Text("aim::%d", aiming);
 	ImGui::Text("action::%d", action);
-	ImGui::Text("pat::%d", pat);
 	////ImGui::Unindent();
 	ImGui::End();
 	Object3d::PreDraw();
