79bca74abc428778e4becc8ec8f09bee110c8518
koyamakaito <k020g1197@g.neec.ac.jp>
2022-05-11 19:18:19 +0900

    病んだぴえん

--- enemy/Rice.h
+++ enemy/Rice.h
@@ -11,6 +11,12 @@ public:
 	void Update()override;
 	void Demo(int num=0);
 	void Draw() override;
+	void SetHit(const bool& hit) { this->hit = hit; }
+	const bool& GetHit() { return hit; };
+	const bool& GetStart() { return start; };
+	void SetExP(const XMFLOAT3& exp) { this->exP = exp; }
+
+	void Rebound();
 private:
 	bool collidePlayer() override;
 	bool collideAttackArm() override;
@@ -19,6 +25,11 @@ private:
 	void Move();
 	float speed_y = 3.0f/20.0f;
 	const float gravity = speed_y/20.0f;
-
+	float speed_y2 = 3.0f / 20.0f;
+	const float gravity2 = speed_y / 20.0f;
+	bool stun = false;
+	bool hit=false;
+	bool start = false;
+	XMFLOAT3 exP{};
 };
 
