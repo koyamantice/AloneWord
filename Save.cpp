ca776fc52dabd28b4f43ac5ef22f196d49242e70
koyamakaito <k020g1197@g.neec.ac.jp>
2022-06-01 12:10:15 +0900

    お疲れ様でした

--- Save.cpp
+++ Save.cpp
@@ -1,11 +1,11 @@
 #include"Save.h"
 
 //静的メンバ変数の実態
-int Save::ClearCount = 0;
-bool Save::FirstClear = false;
-bool Save::SecondClear = false;
-bool Save::ThirdClear = false;
-bool Save::FouthClear = false;
+int Save::ClearCount = 4;
+bool Save::FirstClear = true;
+bool Save::SecondClear = true;
+bool Save::ThirdClear = true;
+bool Save::FouthClear = true;
 bool Save::FifthClear = false;
 bool Save::PerfectClear = false;
 
